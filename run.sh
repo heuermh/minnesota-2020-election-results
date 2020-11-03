2190e14fe226b4e265fc0063eff9e72a273c0c4a
Michael L Heuer <heuermh@acm.org>
2020-11-03 01:25:51 -0600

    Update at Tue Nov  3 01:25:51 CST 2020

--- run.sh
+++ run.sh
@@ -13,8 +13,27 @@ wget \
 wget \
     --mirror \
     --no-parent \
-    --accept=txt,136 \
     "https://electionresults.sos.state.mn.us/Select/MediaFiles/Index?ersElectionId=136"
+
+wget https://electionresultsfiles.sos.state.mn.us/20201103/USPres.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/USPresCG.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/USPresLG.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/USPresCty.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/USPresPct.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/ussenate.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/ussenatecg.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/ussenatecty.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/ussenatepct.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/ushouse.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/USHouseCty.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/ushousepct.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/stsenate.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/StateSenateByPct.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/LegislativeByDistrict.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/sthousepct.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/allracesbycounty.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/allracesbyprecinct.txt
+wget https://electionresultsfiles.sos.state.mn.us/20201103/pctstats.txt
 
 TIMESTAMP=`date`
 git commit -a -m "Update at $TIMESTAMP"
